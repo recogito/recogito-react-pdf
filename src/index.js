8cd81b580c57b9ffc03c100278d097c1c81eb2f8
Rainer Simon <aboutgeo@gmail.com>
2021-09-14 08:15:02 +0200

    Basic PDF rendering with PDF.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
+const PDFJS = require('pdfjs-dist/webpack');
+
+const URL = 'compressed.tracemonkey-pldi-09.pdf';
+
 window.onload = function() {
   
-  // TODO
+  PDFJS.getDocument(URL).promise.then(function(pdf) {
+    const pageNumber = 1;
+
+    pdf.getPage(pageNumber).then(function(page) {          
+      const scale = 1.5;
+      const viewport = page.getViewport({ scale });
+
+      // Prepare canvas using PDF page dimensions
+      const canvas = document.createElement('canvas');
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      document.getElementById('app').appendChild(canvas);
+
+      // Render PDF page into canvas context
+      const renderContext = {
+        canvasContext: canvas.getContext('2d'),
+        viewport
+      };
+
+      page.render(renderContext).promise.then(function () {
+        console.log('Page rendered');
+      });
+      
+    });
+
+  }, function(error) {
+    // Error loading PDF
+    console.error(error);
+  });
 
 }
