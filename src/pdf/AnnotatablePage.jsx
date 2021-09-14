989d4090150145e10058b2e7860857f2003ae5bc
Rainer Simon <aboutgeo@gmail.com>
2021-09-14 15:08:14 +0200

    Annotation layer cleanup on page change

--- src/pdf/AnnotatablePage.jsx
+++ src/pdf/AnnotatablePage.jsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
-import AnnotatableTextLayer from './AnnotatableTextLayer';
+import * as PDFJS from 'pdfjs-dist/webpack';
+import { Recogito } from '@recogito/recogito-js';
+
+import 'pdfjs-dist/web/pdf_viewer.css';
+import '@recogito/recogito-js/dist/recogito.min.css';
 
 const AnnotatablePage = props => {
 
   const containerEl = useRef();
 
-  const [ viewport, setViewport ] = useState();
+  const [ recogito, setRecogito ] = useState();
 
   useEffect(() => {
+    // Clean up previous Recogito instance, if any
+    if (recogito)
+      recogito.destroy();
+
+    const prevCanvas = containerEl.current.querySelector('canvas');
+    if (prevCanvas) {
+      containerEl.current.removeChild(prevCanvas);
+      containerEl.current.querySelector('.textLayer').innerHTML = '';
+    }
+
     if (props.page) {
       const scale = 1.8;
       const viewport = props.page.getViewport({ scale });
-
-      setViewport(viewport);
 
       // Prepare canvas using PDF page dimensions
       const canvas = document.createElement('canvas');
@@ -28,13 +40,27 @@ const AnnotatablePage = props => {
 
       props.page.render(renderContext).promise.then(function () {
         console.log('Page rendered');
+
+        const r = new Recogito({ content: containerEl.current.querySelector('.textLayer'), mode: 'pre' });
+        setRecogito(r)
+      });
+
+      props.page.getTextContent().then(textContent => {
+        PDFJS.renderTextLayer({
+          textContent: textContent,
+          container: containerEl.current.querySelector('.textLayer'),
+          viewport: viewport,
+          textDivs: []
+        }).promise.then(() => {
+          // Do nothing
+        });
       });
     }
   }, [ props.page ]);
 
   return (
     <div ref={containerEl} className="page-container">
-      <AnnotatableTextLayer page={props.page} viewport={viewport} />
+      <div className={props.debug ? 'textLayer debug' : 'textLayer'} />
     </div>
   )
 
--- src/pdf/PDFViewer.jsx
+++ src/pdf/PDFViewer.jsx
@@ -11,6 +11,8 @@ const PDFViewer = props => {
 
   const [ page, setPage ] = useState();
 
+  const [ debug, setDebug ] = useState(false);
+
   useEffect(() => {
     // Load document
     PDFJS.getDocument(props.url).promise.then(pdf => { 
@@ -23,17 +25,47 @@ const PDFViewer = props => {
 
   useEffect(() => {
     if (pdf) {
-      const pageNumber = 1;
-
-      pdf.getPage(pageNumber).then(function(page) { 
+      pdf.getPage(1).then(function(page) { 
         setPage(page);   
       });
     }
   }, [ pdf ]);
 
+  const onToggleDebug = () => 
+    setDebug(!debug);
+
+  const onPreviousPage = () => {
+    const { pageNumber } = page;
+
+    const prevNum = Math.max(0, pageNumber - 1);
+
+    if (prevNum !== pageNumber)
+      pdf.getPage(prevNum).then(page => setPage(page));
+  }
+
+  const onNextPage = () => {
+    const { numPages } = pdf;
+    const { pageNumber } = page;
+
+    const nextNum = Math.min(pageNumber + 1, numPages);
+
+    if (nextNum !== pageNumber)
+      pdf.getPage(nextNum).then(page => setPage(page));
+  }
+
   return (
-    <div className="pdf-viewer-container">
-      <AnnotatablePage page={page} />
+    <div>
+      <header>
+        <button onClick={onToggleDebug}>Toggle Text Layer</button>
+        <button onClick={onPreviousPage}>Previous</button>
+        <label>{page?.pageNumber} / {pdf?.numPages}</label>
+        <button onClick={onNextPage}>Next</button>
+      </header>
+      <main>
+        <div className="pdf-viewer-container">
+          <AnnotatablePage page={page} debug={debug} />
+        </div>
+      </main>
     </div>
   )
 
